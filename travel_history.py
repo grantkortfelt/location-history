22f0921aef173b3cfcd2db9beaf5f46b7637696c
grantkortfelt <grantmail3301@gmail.com>
2025-03-20 02:09:42 -0400

    Add travel_history
    LocationHistory functionality not complete yet

---
+++ travel_history.py
@@ -0,0 +1,72 @@
+# this class contains all variables relative to a location
+class Location:
+    def __init__(self, x, y, info):
+        self.x = x
+        self.y = y
+        self.info = info
+    
+    def coordinates(self):
+        return (self.x, self.y)
+    
+    def contains_info(self):
+        return self.info != None
+
+
+# this class is used to store the location history of a user
+# it contains the user name, location class instance, and the timestamp of the visit
+class LocationHistory:
+    def __init__(self, user, location, timestamp):
+        self.user = user
+        self.location = location
+        self.timestamp = timestamp
+    
+    def get_user(self):
+        return self.user
+    
+    def get_location(self):
+        return self.location
+    
+    def get_timestamp(self):
+        return self.timestamp
+
+
+# this class is used to store the travel history of a user
+# it contains a list of LocationHistory instances
+class TravelHistory:
+    def __init__(self):
+        self.location_histories = []
+    
+    # Add a location to the travel history
+    def add_history(self, location_history):
+        self.location_histories.append(location_history)
+    
+    # return a list of unique locations visited
+    def get_unique_locations(self):
+        pass
+    
+    # return a list of the most visited locations
+    # if there are multiple locations with the same number of visits, return a list of those locations
+    def get_most_visited_locations(self):
+        pass
+    
+    # return dict of every visited location and its number of visits
+    def get_visit_counts(self):
+        pass
+
+
+
+
+locations = []
+location_histories = []
+travel_history = TravelHistory()
+
+# this function generates a location object
+def generate_location(x, y, info):
+    locations.append(Location(x, y, info))
+    return locations[-1]
+
+# this function generates a location history object
+def generate_location_history(location, timestamp):
+    location_histories.append(LocationHistory(location, timestamp))
+    travel_history.add_history(location_histories[-1].location) # update travel history
+    return location_histories[-1]
